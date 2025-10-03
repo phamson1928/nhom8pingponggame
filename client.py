4539914cc356d14451f1f98d7edcbd50cd433a14
sonpham1928 <pson4282@gmail.com>
2025-10-04 02:32:06 +0700

    Commit #6: progress update

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-import socket, json
+import pygame
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
 def main():
-    s = socket.socket()
-    s.connect(("localhost",8080))
-    buffer = ""
-    while True:
-        data = s.recv(1024).decode()
-        buffer += data
-        if "\\n" in buffer:
-            packet, buffer = buffer.split("\\n", 1)
-            print("Received:", packet)
+    running = True
+    while running:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
