ff451c113ff960e40b0863e2074ba8d2b0b7d11f
sonpham1928 <pson4282@gmail.com>
2025-10-04 04:32:06 +0700

    Commit #5: progress update

--- client.py
+++ client.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
-print("Client ready to render paddles and ball")
+import socket, json
+
+def main():
+    s = socket.socket()
+    s.connect(("localhost",8080))
+    buffer = ""
+    while True:
+        data = s.recv(1024).decode()
+        buffer += data
+        if "\\n" in buffer:
+            packet, buffer = buffer.split("\\n", 1)
+            print("Received:", packet)
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-import socket, threading, random, time
-
-WIDTH, HEIGHT = 800, 600
+import socket, threading, json, time
 
 class GameServer:
     def __init__(self):
-        self.paddles = {0: 250, 1: 250}
-        self.ball = {"x": WIDTH//2, "y": HEIGHT//2}
-
-def main():
-    gs = GameServer()
-    print("Initial state:", gs.ball)
+        self.paddles = {0:250, 1:250}
+        self.ball = {"x":400, "y":300}
+    def run(self):
+        s = socket.socket()
+        s.bind(("localhost",8080))
+        s.listen(2)
+        conn, _ = s.accept()
+        while True:
+            state = json.dumps({"paddles": self.paddles, "ball": self.ball}) + "\\n"
+            conn.sendall(state.encode())
+            time.sleep(0.1)
 
 if __name__ == "__main__":
-    main()
+    GameServer().run()
