990a0eed445ad35f4676a1beb56fa2ec9b979394
sonpham1928 <pson4282@gmail.com>
2025-10-04 19:03:12 +0700

    Fix lost code

--- client.py
+++ client.py
@@ -1,19 +1,109 @@
-#!/usr/bin/env python3
-import pygame
-pygame.init()
+from pygame import *
+import socket
+import json
+from threading import Thread
+
 WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
+init()
+screen = display.set_mode((WIDTH, HEIGHT))
+clock = time.Clock()
+display.set_caption("Пінг-Понг")
+def connect_to_server():
+    while True:
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(('localhost', 8080)) 
+            buffer = ""
+            game_state = {}
+            my_id = int(client.recv(24).decode())
+            return my_id, game_state, buffer, client
+        except:
+            pass
 
-def main():
-    running = True
-    while running:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                running = False
+
+def receive():
+    global buffer, game_state, game_over
+    while not game_over:
+        try:
+            data = client.recv(1024).decode()
+            buffer += data
+            while "\n" in buffer:
+                packet, buffer = buffer.split("\n", 1)
+                if packet.strip():
+                    game_state = json.loads(packet)
+        except:
+            game_state["winner"] = -1
+            break
+
+
+font_win = font.Font(None, 72)
+font_main = font.Font(None, 36)
+
+game_over = False
+winner = None
+you_winner = None
+my_id, game_state, buffer, client = connect_to_server()
+Thread(target=receive, daemon=True).start()
+while True:
+    for e in event.get():
+        if e.type == QUIT:
+            exit()
+
+    if "countdown" in game_state and game_state["countdown"] > 0:
         screen.fill((0, 0, 0))
-        pygame.display.flip()
-        clock.tick(60)
+        countdown_text = font.Font(None, 72).render(str(game_state["countdown"]), True, (255, 255, 255))
+        screen.blit(countdown_text, (WIDTH // 2 - 20, HEIGHT // 2 - 30))
+        display.update()
+        continue 
 
-if __name__ == "__main__":
-    main()
+    if "winner" in game_state and game_state["winner"] is not None:
+        screen.fill((20, 20, 20))
+
+        if you_winner is None:
+            if game_state["winner"] == my_id:
+                you_winner = True
+            else:
+                you_winner = False
+
+        if you_winner:
+            text = "Ти переміг!"
+        else:
+            text = "Пощастить наступним разом!"
+
+        win_text = font_win.render(text, True, (255, 215, 0))
+        text_rect = win_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        screen.blit(win_text, text_rect)
+
+        text = font_win.render('К - рестарт', True, (255, 215, 0))
+        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 120))
+        screen.blit(text, text_rect)
+
+        display.update()
+        continue 
+
+    if game_state:
+        screen.fill((30, 30, 30))
+        draw.rect(screen, (0, 255, 0), (20, game_state['paddles']['0'], 20, 100))
+        draw.rect(screen, (255, 0, 255), (WIDTH - 40, game_state['paddles']['1'], 20, 100))
+        draw.circle(screen, (255, 255, 255), (game_state['ball']['x'], game_state['ball']['y']), 10)
+        score_text = font_main.render(f"{game_state['scores'][0]} : {game_state['scores'][1]}", True, (255, 255, 255))
+        screen.blit(score_text, (WIDTH // 2 -25, 20))
+
+        if game_state['sound_event']:
+            if game_state['sound_event'] == 'wall_hit':
+                pass
+            if game_state['sound_event'] == 'platform_hit':
+                pass
+
+    else:
+        wating_text = font_main.render(f"Очікування гравців...", True, (255, 255, 255))
+        screen.blit(wating_text, (WIDTH // 2 - 25, 20))
+
+    display.update()
+    clock.tick(60)
+
+    keys = key.get_pressed()
+    if keys[K_w]:
+        client.send(b"UP")
+    elif keys[K_s]:
+        client.send(b"DOWN")
