4d2cd0973c8926a18ad7797c9d11c212c606816f
sonpham1928 <pson4282@gmail.com>
2025-10-04 16:32:06 +0700

    Commit #1: progress update

---
+++ client.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# client.py - initial setup
+print("Starting Pong Client...")
---
+++ server.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# server.py - initial setup
+print("Starting Pong Server...")
